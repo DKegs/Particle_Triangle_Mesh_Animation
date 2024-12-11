6218d3dceafcdabe04287894b797dd0942497c1a
Daniel Keogler <98786287+DKegs@users.noreply.github.com>
2024-12-11 14:27:44 -0500

    ParticleMeshAnimation.py
    Added:
    - Better particle movement to mimic Sebastian Lague's Intro
    - Comments
    - Better mesh coloring system
    - Improved particle class by adding more functions
    - Improved readability
    - Simplified main loop

--- ParticleMeshAnimation.py
+++ ParticleMeshAnimation.py
@@ -9,23 +9,25 @@ from scipy.spatial import Delaunay
 
 pygame.init()
 clock = pygame.time.Clock()
-backgroundColor = (104, 112, 152) 
 particleColor = (0, 0, 0)
-lineColor = (0, 255, 0)
 screenXsize = 1000
 screenYsize = 800
 screen = pygame.display.set_mode((screenXsize, screenYsize)) 
 pygame.display.set_caption('Floating Point Animation') 
-screen.fill(backgroundColor) 
 pygame.display.update()
 
 # Define how many particles will be in the animation
 particleCount = 100
 
+# For new colors on each run
+offsetR = random.randint(0, 100) # Red
+offsetG = random.randint(0, 100) # Green
+offsetB = random.randint(0, 100) # Blue
+
 
 
 # Create particle class
-# This class will handle the particle movement
+# This class will handle the particle creation, drawing, and movement
 class Particle:
     def __init__(self, color: tuple , x: float, y: float, radius: float, speed: float, angle: float):
         self.color = color
@@ -57,6 +59,7 @@ class Particle:
         #         self.y = 0
 
     # Adds the proper movement each frame
+        self.angle += ((random.random() - .5) / 5)
         self.x += round(self.speed * math.cos(self.angle), 1)
         self.y += round(self.speed * math.sin(self.angle), 1)
 
@@ -67,79 +70,84 @@ class Particle:
 
 
 # DrawPoints redraws the particles every frame in their new positions
-
     def drawPoints(self):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius, 0)
 
+# Creates the Delaunay Triangle Mesh using SciPy
+    def getTriangleMeshFromPoints(self, allPoints):
+        return (Delaunay(allPoints))
+    
+# Uses the Delaunay Triangle Mesh to draw the mesh onto the screen, then colors each triangle according to its height
+    def drawTriangleMesh(self, allTriangles, allPoints, heightRatio):
+        for triangle in allTriangles.simplices:
+            trinaglePoints = allPoints[triangle]
+            averageYofTriangle = 0
+            
+            for points in trinaglePoints:
+                averageYofTriangle += points[1]
+            
+            averageYofTriangle = (averageYofTriangle / len(trinaglePoints)) * heightRatio
+            pygame.draw.polygon(screen, 
+                               (averageYofTriangle + offsetR, # Red
+                               averageYofTriangle + offsetG,  # Green
+                               averageYofTriangle + offsetB), # Blue
+                               trinaglePoints)
 
 
 
-# The main loads the screen, but waits to start the animation until the user presses the spacebar
-# Once pressed, it loads the particles based on how many are set in the particleCount integer
-# It then calls the particle class for drawing them on the screen and moving them accordingly
-# The closest particles list is used to create the shapes between particles for the animation
+
+# The main loads the screen, and appends all the particles based on particleCount
+# While running loop gets all particle positions and (x, y) tuples, calls getTriangleMeshFromPoints, then calls DrawTriangleMesh
+# Finally it updates all positions of the particles and updates the screen, then repeats all steps
 
 def main():
-    
+    # Set up initial conditions before animation loop
     running = True
-    # game loop 
-    particleExists = False
+
+    # Add corner particles
+    particles = []
+    particles.append(Particle(particleColor, 0, 0, 1, 0, 0))
+    particles.append(Particle(particleColor, screenXsize, 0, 1, 0, 0))
+    particles.append(Particle(particleColor, 0, screenYsize, 1, 0, 0))
+    particles.append(Particle(particleColor, screenXsize, screenYsize, 1, 0, 0))
+    
+    for i in range(particleCount - 4): # Removes 4 Corner particles from count
+        particle = Particle(
+            particleColor,                  # Color
+            random.randint(0, screenXsize), # X location
+            random.randint(0, screenYsize), # Y location
+            random.randint(1,8),            # Size
+            (random.random() * .5),         # Speed
+            random.uniform(0, 2 * math.pi)) # Angle
+        particles.append(particle)
+        particle.drawPoints()
+
+
+
+    # animation loop 
     while running: 
-    # for loop through the event queue   
-        for event in pygame.event.get():       
-            # Check for QUIT event       
-            if event.type == pygame.QUIT: 
+     
+        for event in pygame.event.get(): # For loop through the event queue             
+            if event.type == pygame.QUIT: # Check for QUIT event
                 running = False
+
+        clock.tick(60) # 60 FPS
+
+        allParticlePositions = [] # Get intial list of all X and Y coordinates as tuples: (x, y)
+
+        for i in range(particleCount): # Calling all particle positions 
+            particle = particles[i]
+            allParticlePositions.append(particle.getXY())
         
+        allTriangles = particle.getTriangleMeshFromPoints(allParticlePositions) # Create mesh out of allParticlePositions using Delaunay
+        heightRatio = 150/screenYsize # Create screen height ratio for proper triangle coloring
+        particle.drawTriangleMesh(allTriangles, np.array(allParticlePositions), heightRatio) # Draw the mesh
 
-        keys = pygame.key.get_pressed()
-
-        
-        if keys[pygame.K_SPACE]:
-            particles = []
-            particles.append(Particle(particleColor, 0, 0, 1, 0, 0))
-            particles.append(Particle(particleColor, screenXsize, 0, 1, 0, 0))
-            particles.append(Particle(particleColor, 0, screenYsize, 1, 0, 0))
-            particles.append(Particle(particleColor, screenXsize, screenYsize, 1, 0, 0))
-            for i in range(particleCount - 4):
-                particle = Particle(
-                    particleColor,                  # Color
-                    random.randint(0, screenXsize), # X location
-                    random.randint(0, screenYsize), # Y location
-                    random.randint(1,10),           # Size
-                    (random.random() * .25),        # Speed
-                    random.uniform(0, 2 * math.pi)) # Angle
-                particles.append(particle)
-                particle.drawPoints()
-            particleExists = True
-            
-
-        if particleExists:
-            clock.tick(60) # 60 FPS
-            allParticlePositionsXY = []
-
-            screen.fill(backgroundColor)
-            for i in range(particleCount):  
-                particle = particles[i]
-                allParticlePositionsXY.append(particle.getXY())
-            
-            allPoints = np.array(allParticlePositionsXY)
-            allTriangles = Delaunay(allPoints)
-            heightRatio = 255/screenYsize
-            
-            for triangle in allTriangles.simplices:
-                trinaglePoints = allPoints[triangle]
-                averageYofTriangle = 0
-                for points in trinaglePoints:
-                    averageYofTriangle += points[1]
-                averageYofTriangle = (averageYofTriangle / len(trinaglePoints)) * heightRatio
-                pygame.draw.polygon(screen, (0, 0, averageYofTriangle), trinaglePoints)
-
-            for i in range(particleCount): 
-                particle = particles[i]
-                particle.update()
-                particle.drawPoints()
-            pygame.display.update()
+        for i in range(particleCount): # Update the screen
+            particle = particles[i]
+            particle.update()
+            particle.drawPoints()
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
